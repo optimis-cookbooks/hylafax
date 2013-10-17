ea75a9304f061f14ea4dbd49700096defd1facf2
Umang Chouhan <uchouhan@optimiscorp.com>
2013-10-18 03:47:12 +0530

    Updating client recipe for hylafax client settings.

--- recipes/client.rb
+++ recipes/client.rb
@@ -1,1 +1,7 @@
 package 'hylafax-client'
+
+template '/etc/hylafax/hyla.conf' do
+  source 'hyla.conf.erb'
+  owner 'root'
+  group 'root'
+end
--- spec/recipes/client_spec.rb
+++ spec/recipes/client_spec.rb
@@ -1,7 +1,11 @@
 require 'chefspec'
 
 describe 'hylafax::client' do
-  let(:chef_run) { ChefSpec::ChefRunner.new('..').converge 'hylafax::client' }
+  let(:chef_run) { ChefSpec::Runner.new.converge described_recipe }
 
   it { chef_run.should install_package 'hylafax-client' }
+
+  it 'should configure hylafax client' do
+    expect(chef_run).to render_file '/etc/hylafax/hyla.conf'
+  end
 end
